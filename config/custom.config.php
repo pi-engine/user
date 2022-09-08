260e2e2ffe0ea0c55f127e69b930f29ea23a7247
Hossein Azizabadi Farahani <hossein@azizabadi.com>
2022-09-08 16:21:03 +0430

    Connect by WC

--- config/custom.config.php
+++ config/custom.config.php
@@ -2,7 +2,7 @@
 
 return [
     'jwt' => [
-        'secret'      => 'xt2468xc9mh5hvnal80rng36bbk16co4',
+        'secret'      => '',
         //'exp_access'  => 900, // 15 min
         'exp_access'  => 1209600, // 14 days, for development
         'exp_refresh' => 7776000, // 90 days
