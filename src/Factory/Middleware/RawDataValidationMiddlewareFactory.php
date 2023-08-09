d33990e2756a43301c0338ca22377e902fcbc28c
kerloper <karimijavad990@gmail.com>
2023-08-09 12:47:23 +0330

    handle validate in raw data request

---
+++ src/Factory/Middleware/RawDataValidationMiddlewareFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace User\Factory\Middleware;
+
+use Interop\Container\ContainerInterface;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use User\Handler\ErrorHandler;
+use User\Middleware\ValidationMiddleware;
+use User\Service\AccountService;
+use User\Service\CacheService;
+
+class RawDataValidationMiddlewareFactory implements FactoryInterface
+{
+    /**
+     * @param ContainerInterface $container
+     * @param string             $requestedName
+     * @param null|array         $options
+     *
+     * @return ValidationMiddleware
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null): ValidationMiddleware
+    {
+        return new ValidationMiddleware(
+            $container->get(ResponseFactoryInterface::class),
+            $container->get(StreamFactoryInterface::class),
+            $container->get(AccountService::class),
+            $container->get(CacheService::class),
+            $container->get(ErrorHandler::class)
+        );
+    }
+}
