6e589963d02e0e6744f686038212ff7a14f6934c
kerloper <karimijavad990@gmail.com>
2023-11-05 10:08:41 +0330

    check duplicate value in add role

--- src/Handler/Admin/Role/AddHandler.php
+++ src/Handler/Admin/Role/AddHandler.php
@@ -9,6 +9,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use stdClass;
 use User\Service\RoleService;
 
 class AddHandler implements RequestHandlerInterface
@@ -36,11 +37,33 @@ class AddHandler implements RequestHandlerInterface
     {
         $requestBody    = $request->getParsedBody();
         $operator       = $request->getAttribute('account');
-        $result         = $this->roleService->addRoleResource($requestBody,$operator);
+
+        $list           = $this->roleService->getRoleResourceList();
+        $isDuplicate    = false;
+        foreach ($list as $item) {
+            if ($item["name"] === $requestBody['name']??'') {
+                $isDuplicate = true;
+                break; // Exit the loop once a match is found
+            }
+        }
+        if($isDuplicate||!isset($requestBody['name'])||empty($requestBody['name'])){
+            return new JsonResponse(
+                [
+                    'result' => false,
+                    'data'   => new stdClass(),
+                    'error'  => [
+                        'message'   => 'Bad request!',
+                        'code'      => 400
+                    ],
+                ]
+            );
+        }
+
+//        $result         = $this->roleService->addRoleResource($requestBody,$operator);
         return new JsonResponse(
             [
                 'result' => true,
-                'data'   => $result,
+                'data'   => $list,
                 'error'  => [],
             ]
         );
