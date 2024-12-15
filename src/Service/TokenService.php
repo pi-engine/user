91dcbde4c4a069db80b67230e01f87054875e6a6
Hossein Azizabadi Farahani <hossein.azizabadi@gmail.com>
2024-12-15 16:14:57 +0330

    Improve token service security

--- src/Service/TokenService.php
+++ src/Service/TokenService.php
@@ -26,31 +26,29 @@ class TokenService implements ServiceInterface
 
     public function encryptToken($params): array
     {
-        // Set key
+        // Generate a unique ID
         $uniqId = $this->setUniqId($params);
 
-        // Set payload
+        // Build the payload
         $payload = [
             'id'   => $uniqId,
             'uid'  => $params['account']['id'],
             'iat'  => time(),
             'exp'  => $params['exp'] ?? time() + $this->setTtl($params),
             'type' => $params['type'],
-            'iss'  => $this->config['iss'],
-            'aud'  => $this->config['aud'],
+            'iss'  => $this->config['iss'] ?? '',
+            'aud'  => $this->config['aud'] ?? '',
             'sub'  => sprintf('user-%s', $params['account']['id']),
         ];
 
-        // Set additional information in the payload
+        // Add additional payload fields if configured
         if (isset($this->config['additional']) && !empty($this->config['additional'])) {
-            foreach ($this->config['additional'] as $additional) {
-                $payload[$additional] = '';
-                if (isset($params['account'][$additional])) {
-                    $payload[$additional] = $params['account'][$additional];
-                }
+            foreach ($this->config['additional'] as $key) {
+                $payload[$key] = $params['account'][$key] ?? '';
             }
         }
 
+        // Generate and return the token
         return [
             'token'   => JWT::encode($payload, $this->setEncryptKey(), $this->setAlgorithm()),
             'key'     => $uniqId,
@@ -61,43 +59,85 @@ class TokenService implements ServiceInterface
     public function decryptToken($token): array
     {
         try {
-            $decoded = JWT::decode($token, new Key($this->setDecryptKey(), $this->setAlgorithm()));
+            // Decode the token
+            $decodedToken = JWT::decode(
+                $token,
+                new Key($this->setDecryptKey(), $this->setAlgorithm())
+            );
 
-            // Get data from cache
-            $cacheUser = $this->cacheService->getUser((int)$decoded->uid);
-
+            // Validate token structure
             if (
-                !empty($decoded)
-                && !empty($cacheUser)
-                && $decoded->type == 'access'
-                && in_array($decoded->id, array_keys($cacheUser['access_keys']))
+                empty($decodedToken) ||
+                !is_int($decodedToken->uid ?? null) ||
+                !in_array($decodedToken->type ?? '', ['access', 'refresh'], true) ||
+                !is_string($decodedToken->id ?? null)
             ) {
-                return [
-                    'status'  => true,
-                    'id'      => $decoded->id,
-                    'user_id' => $decoded->uid,
-                    'type'    => $decoded->type,
-                    'data'    => (array)$decoded,
-                ];
-            } elseif (
-                !empty($decoded)
-                && !empty($cacheUser)
-                && $decoded->type == 'refresh'
-                && in_array($decoded->id, array_keys($cacheUser['refresh_keys']))
-            ) {
-                return [
-                    'status'  => true,
-                    'id'      => $decoded->id,
-                    'user_id' => $decoded->uid,
-                    'type'    => $decoded->type,
-                    'data'    => (array)$decoded,
-                ];
-            } else {
                 return [
                     'status'  => false,
-                    'message' => 'Token not valid !',
+                    'message' => 'Invalid token structure.',
                 ];
             }
+
+            // Additional expire time check
+            if ($decodedToken->exp < time()) {
+                return [
+                    'status'  => false,
+                    'message' => 'Token expired.',
+                ];
+            }
+
+            // Add Issued-At Skew
+            if ($decodedToken->iat > (time() + 60)) {
+                return [
+                    'status'  => false,
+                    'message' => 'Invalid issued time.',
+                ];
+            }
+
+            // Validate the 'iss' (Issuer) and 'aud' (Audience) claims
+            if ($decodedToken->iss !== $this->config['iss'] || $decodedToken->aud !== $this->config['aud']) {
+                return [
+                    'status'  => false,
+                    'message' => 'Invalid issuer or audience',
+                ];
+            }
+
+            // Get and check user data from cache
+            $user = $this->cacheService->getUser((int)$decodedToken->uid);
+            if (empty($user)) {
+                return [
+                    'status'  => false,
+                    'message' => 'User not found.',
+                ];
+            }
+
+            // Determine token type and validate access
+            $isValidAccessToken = (
+                $decodedToken->type === 'access' &&
+                isset($user['access_keys'][$decodedToken->id])
+            );
+
+            // Determine token type and validate access
+            $isValidRefreshToken = (
+                $decodedToken->type === 'refresh' &&
+                isset($user['refresh_keys'][$decodedToken->id])
+            );
+
+            // Check token is valid or not
+            if ($isValidAccessToken || $isValidRefreshToken) {
+                return [
+                    'status'  => true,
+                    'id'      => $decodedToken->id,
+                    'user_id' => $decodedToken->uid,
+                    'type'    => $decodedToken->type,
+                    'data'    => (array)$decodedToken,
+                ];
+            }
+
+            return [
+                'status'  => false,
+                'message' => 'Token not valid!',
+            ];
         } catch (Exception $e) {
             return [
                 'status'  => false,
@@ -108,76 +148,46 @@ class TokenService implements ServiceInterface
 
     private function setUniqId($params): string
     {
-        switch ($params['type']) {
-            default:
-            case 'access':
-                return hash(
-                    'sha256',
-                    sprintf(
-                        'a-%s-%s',
-                        $params['account']['id'],
-                        Rand::getString('16', 'abcdefghijklmnopqrstuvwxyz0123456789')
-                    )
-                );
+        $typePrefix = $params['type'] === 'refresh' ? 'r' : 'a';
+        $randomString = Rand::getString(16, 'abcdefghijklmnopqrstuvwxyz0123456789');
 
-            case 'refresh':
-                return hash(
-                    'sha256',
-                    sprintf(
-                        'r-%s-%s',
-                        $params['account']['id'],
-                        Rand::getString('16', 'abcdefghijklmnopqrstuvwxyz0123456789')
-                    )
-                );
-        }
+        return hash(
+            'sha256',
+            sprintf('%s-%s-%s', $typePrefix, $params['account']['id'], $randomString)
+        );
     }
 
-    private function setTtl($params): int
+    private function setTtl(array $params): int
     {
-        switch ($params['type']) {
-            default:
-            case 'access':
-                return (int)$this->config['exp_access'];
-
-            case 'refresh':
-                return (int)$this->config['exp_refresh'];
-        }
+        $type = $params['type'] === 'refresh' ? 'exp_refresh' : 'exp_access';
+        return (int)($this->config[$type] ?? 0);
     }
 
     private function setAlgorithm(): string
     {
-        if (isset($this->config['type']) && $this->config['type'] == 'keys') {
-            return 'RS256';
-        }
-
-        return 'HS256';
+        return ($this->config['type'] ?? '') === 'keys' ? 'RS256' : 'HS256';
     }
 
-    private function setEncryptKey()
+    private function setEncryptKey(): string
+    {
+        return $this->getKey('private_key');
+    }
+
+    private function setDecryptKey(): string
+    {
+        return $this->getKey('public_key');
+    }
+
+    private function getKey(string $keyType): string
     {
         if (
-            isset($this->config['type'])
-            && $this->config['type'] == 'keys'
-            && isset($this->config['private_key'])
-            && !empty($this->config['private_key'])
+            ($this->config['type'] ?? '') === 'keys' &&
+            !empty($this->config[$keyType] ?? null)
         ) {
-            return file_get_contents($this->config['private_key']);
+            return file_get_contents($this->config[$keyType]);
         }
 
         return $this->config['secret'];
     }
 
-    private function setDecryptKey()
-    {
-        if (
-            isset($this->config['type'])
-            && $this->config['type'] == 'keys'
-            && isset($this->config['public_key'])
-            && !empty($this->config['public_key'])
-        ) {
-            return file_get_contents($this->config['public_key']);
-        }
-
-        return $this->config['secret'];
-    }
 }
