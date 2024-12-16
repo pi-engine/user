371e7e050fd4eaeec47929f4a9b693f988634147
Hossein Azizabadi Farahani <hossein.azizabadi@gmail.com>
2024-12-16 10:17:13 +0330

    Add language files

---
+++ language/ar_AE.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'add-account'             => 'إنشاء حساب جديد',
+    'add-account-message'     => 'تم إنشاء حساب جديد بمعلوماتك على منصتنا!',
+    'update-account'          => 'تحديث الحساب',
+    'update-account-message'  => 'تم تحديث بيانات حسابك!',
+    'delete-account'          => 'حذف الحساب',
+    'delete-accounts-message' => 'تم حذف حسابك!',
+    'update-password'         => 'تحديث كلمة مرور الحساب',
+    'update-password-message' => 'تم تحديث كلمة مرور حسابك بواسطة المسؤول!',
+    'update-roles'            => 'تحديث أدوار الحساب',
+    'update-roles-message'    => 'تم تحديث أدوار حسابك بواسطة المسؤول!',
+    'update-status'           => 'تحديث حالة الحساب',
+    'update-status-message'   => 'تم تحديث حالة حسابك بواسطة المسؤول!',
+];
