3ecbe37e7f1f03f6d3fbb74af181e3d7b57ecbcd
Hossein Azizabadi Farahani <hossein.azizabadi@gmail.com>
2024-12-16 10:15:53 +0330

    Add language files

---
+++ language/en_US.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'add-account'             => 'New account created',
+    'add-account-message'     => 'New account created by your information in our platform !',
+    'update-account'          => 'Update account',
+    'update-account-message'  => 'Your account data updated!',
+    'delete-account'          => 'Delete account',
+    'delete-accounts-message' => 'Your account deleted !',
+    'update-password'         => 'Update account password',
+    'update-password-message' => 'Your account password updated by admin !',
+    'update-roles'            => 'Update account roles',
+    'update-roles-message'    => 'Your account roles updated by admin !',
+    'update-status'           => 'Update account status',
+    'update-status-message'   => 'Your account status updated by admin !',
+];
